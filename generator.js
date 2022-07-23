e90436f9135f9f21b636bba36068ddb14c5efe5a
Артём Кэшов? <81440085+artemcashov@users.noreply.github.com>
2022-07-23 17:45:52 +0300

    чоч

--- generator.js
+++ generator.js
@@ -2,18 +2,7 @@ var fs = require("fs")
 
 var date = [00, 00, 00]
 
-var content = `function prompt(question, callback) {
-    var stdin = process.stdin,
-        stdout = process.stdout;
-
-    stdin.resume();
-    stdout.write(question);
-
-    stdin.once('data', function (data) {
-        callback(data.toString().trim());
-    });
-}
-var fs = require("fs")
+var content = `var fs = require("fs")
 var stdin = process.openStdin();
 var date = new Date();
 var hours = date.getHours()
