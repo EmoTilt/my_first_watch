8f7363550b7f7d45c1040f241a6da5fc95e378b2
Артём Кэшов? <81440085+artemcashov@users.noreply.github.com>
2022-07-24 18:19:53 +0300

    ы

--- generator.js
+++ generator.js
@@ -1,7 +1,5 @@
 var fs = require("fs")
 
-var date = [00, 00, 00]
-
 var content = `var fs = require("fs")
 var stdin = process.openStdin();
 var date = new Date();
@@ -9,9 +7,9 @@ var hours = date.getHours()
 var minutes = date.getMinutes()
 var seconds = date.getSeconds()`
 
-for (let h = date[0]; h < 24; h++) {
-    for (let m = date[1]; m < 60; m++) {
-        for (let s = date[2]; s < 60; s++) {
+for (let h = 0; h < 24; h++) {
+    for (let m = 0; m < 60; m++) {
+        for (let s = 0; s < 60; s++) {
             content += `\nif(hours == ${h} && minutes == ${m} && seconds == ${s}) {\nconsole.log("Ваше время - ${h}:${m}:${s}")\n}`
         }
     }
